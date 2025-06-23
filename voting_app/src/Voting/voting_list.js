828b8fa2338ccbf3eb84494fae58c88f381d6348
Pothe <keoutpothe2014@gmail.com>
2025-06-23 15:48:39 +0700

    Update voting_list.js

--- voting_app/src/Voting/voting_list.js
+++ voting_app/src/Voting/voting_list.js
@@ -1,5 +1,5 @@
 import React from "react";
-import productsData from "./data";
+// import productsData from "./data";
 import Product from "./ProductLIst/Products";
 
 
@@ -8,34 +8,42 @@ class Voting extends React.Component{
 
     render(){
     
-        const ProductListIterm = productsData.map((product)=>{
-            return(
-            <Product key={product.id}
-            title ={product.title}
-            des = {product.description}
-            price ={product.price}
-            // id={product.id}
-            />
-            )
-        })
-    //    const products = [
-    //     {
-    //     id: 1,
-    //     title: 'Yellow Pail',
-    //     description: 'On-demand sand castle construction expertise.',        
-    //     },
-    //      {
-    //     id: 2,
-    //     title: 'Red Pail',
-    //     description: 'On-demand sand castle construction expertise.',        
-    //     },
-    //      {
-    //     id: 3,
-    //     title: 'Black Pail',
-    //     description: 'On-demand sand castle construction expertise.',        
-    //     }
-    // ];
+        // const ProductListIterm = productsData.map((product)=>{
+        //     return(
+        //     <Product key={product.id}
+        //     title ={product.title}
+        //     des = {product.description}
+        //     price ={product.price}          
+        //     />
+        //     )
+        // })
+       const products = [
+        {
+        id: 1,
+        title: 'Yellow Pail',
+        description: 'On-demand sand castle construction expertise.',        
+        },
+         {
+        id: 2,
+        title: 'Red Pail',
+        description: 'On-demand sand castle construction expertise.',        
+        },
+         {
+        id: 3,
+        title: 'Black Pail',
+        description: 'On-demand sand castle construction expertise.',        
+        }
+    ];
 // const item = products[0];
+const ProductListIterm = products.map((product)=>{
+    return (
+        <Product
+         key={product.id}
+         title ={product.title}
+         des = {product.description}
+         />
+    )
+})
         return(
             <div>    
             {/* pass data to child component by props*/}            

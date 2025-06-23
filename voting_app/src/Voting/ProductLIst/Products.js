c191fab73eaab413d1415756814518a01ba59d9e
Pothe <keoutpothe2014@gmail.com>
2025-06-23 10:40:33 +0700

    Update Products.js

--- voting_app/src/Voting/ProductLIst/Products.js
+++ voting_app/src/Voting/ProductLIst/Products.js
@@ -8,8 +8,9 @@ class Product extends React.Component{
           <>
       <div className="flex-grow">
         <h3 className="text-lg font-semibold text-gray-800">{item.name}</h3>
-        <p className="text-sm text-gray-500 mb-1">itemNo.</p>
+        <p className="text-sm text-gray-500 mb-1">{item.sex}</p>
         <p className="text-xs text-gray-600">
+            {item.description}
           
         </p>
       </div>

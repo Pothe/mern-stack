bb0c0a6f2ed6fc1739bf83178a40fde57e0c5701
Pothe <keoutpothe2014@gmail.com>
2025-06-30 08:13:50 +0700

    timer tracking app 30/6/25

--- time_tracking_app/src/Components/EditableTimerList.js
+++ time_tracking_app/src/Components/EditableTimerList.js
@@ -5,6 +5,9 @@ class EditableTimerList extends React.Component {
         return (
         <>
           <h1>EditableTimerList</h1>
+          <div className="p-4 text-center bg-blue-500 md:p-8 md:text-left lg:bg-green-500">
+    This div adapts based on screen size.
+</div>
         </>
     );
     }
--- time_tracking_app/src/TimersDashboard.js
+++ time_tracking_app/src/TimersDashboard.js
@@ -1,18 +1,21 @@
 import React from "react";
 import EditableTimerList from "./Components/EditableTimerList";
 import ToggleableTimerForm from "./Components/ToggleableTimerForm";
+import './TimerDashboard.css';
 class TimersDashboard extends React.Component {
     state = {  } 
     render() { 
         return (
-        <>
+       <div className="container">
+        <div className="card">
     <EditableTimerList/>
     
     <ToggleableTimerForm 
     isOpent={true}
     />
-
-        </>);
+    </div>
+       </div>
+       );
     }
 }
  

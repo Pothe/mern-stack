0f2bb4d5b36bbd6b25c0292d6805cfa97c85e599
Pothe <keoutpothe2014@gmail.com>
2025-06-30 09:53:31 +0700

    Create EditableTimer.js

---
+++ time_tracking_app/src/Components/sub_component/EditableTimer.js
@@ -0,0 +1,12 @@
+import React from "react";
+class EditableTimer extends React.Component{
+    render(){
+        return(
+            <>
+            Editable timer
+            </>
+        )
+    }
+}
+
+export default EditableTimer;

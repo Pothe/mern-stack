60868ae4bd591584dfc9277af9f14d70fb1f4343
Pothe <keoutpothe2014@gmail.com>
2025-07-01 15:34:17 +0700

    Update Timer.js

--- time_tracking_app/src/Components/sub_component/Timer.js
+++ time_tracking_app/src/Components/sub_component/Timer.js
@@ -10,7 +10,10 @@ class Timer extends React.Component{
             
           </div>
           <div className="mb-3">
-            <h3 htmlFor="projectInput" className="form-label">{this.props.runningSince}</h3>
+            <h3 htmlFor="projectInput" className="form-label">{this.props.project}</h3>
+                     </div>
+                     <div className="mb-3">
+            <h3 htmlFor="projectInput" className="form-label">{this.props.elapse}</h3>
                      </div>
           <div className="d-flex justify-content-end gap-1" ><i className="bi bi-trash3"></i><i className="bi bi-pencil-square"></i></div>
           <div className="d-grid gap-2 d-md-flex justify-content-md-center mt-4"> {/* Added mt-4 for some top margin */}
